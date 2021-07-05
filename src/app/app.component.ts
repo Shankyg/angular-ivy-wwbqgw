e518e3ac599228d85d44cff4715dce0ccffa9636
Shankyg <shashankg615@gmail.com>
2021-07-06 02:16:58 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,45 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = 'ScoreCard';
+  winCondition: number = 5;
+  resetFlag: boolean = false;
+  winner: string = null;
+  players = [
+    {
+      name: 'player one',
+      score: 0
+    },
+    {
+      name: 'player two',
+      score: 0
+    }
+  ];
+
+  score(index: number) {
+    this.players[index].score++;
+    if (this.players[index].score == this.winCondition) {
+      this.resetFlag = true;
+      this.winner = this.players[index].name;
+    }
+  }
+
+  reset() {
+    this.players = [
+      {
+        name: 'player one',
+        score: 0
+      },
+      {
+        name: 'player two',
+        score: 0
+      }
+    ];
+
+    this.resetFlag = false;
+    this.winner = null;
+  }
 }
